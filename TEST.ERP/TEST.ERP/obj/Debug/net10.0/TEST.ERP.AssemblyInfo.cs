2e070e740df9ad5e3659ac64c9c9b856fad2fda0
Baburao Adkane <b.g.adkane27@gmail.com>
2025-11-26 07:37:52 +0530

    Package Added FluentAssertions

--- TEST.ERP/TEST.ERP/obj/Debug/net10.0/TEST.ERP.AssemblyInfo.cs
+++ TEST.ERP/TEST.ERP/obj/Debug/net10.0/TEST.ERP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEST.ERP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4f386bdd02b76c9118d9a9c07864bf54e98c9df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15763f12585e0bdc93ecf067daebef556f2fab92")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEST.ERP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEST.ERP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
